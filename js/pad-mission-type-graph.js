cf1ec4412e2c8702a4efd4860dd0ddce2d58b6e3
Anders Lindhardt Madsen <andem20@student.sdu.dk>
2022-12-17 21:17:15 +0100

    change to aera visualization

--- js/pad-mission-type-graph.js
+++ js/pad-mission-type-graph.js
@@ -2,7 +2,14 @@ import worldJson from "../assets/world.json" assert { type: "json" };
 import locations from "../data/Locations.json" assert { type: "json" };
 import orbits from "../data/Orbits.json" assert { type: "json" };
 import launches from "../data/Launches.json" assert { type: "json" };
-
+function sortPoints(points) {
+    points = points.splice(0);
+    var p0 = {};
+    p0.y = Math.min.apply(null, points.map(p=>p[1]));
+    p0.x = Math.max.apply(null, points.filter(p=>p.y == p0.y).map(p=>p.x));
+    points.sort((a,b)=>angleCompare(p0, a, b));
+    return points;
+};
 
 const concatPadLocations = [];
 
@@ -114,36 +121,77 @@ option = {
     animationDurationUpdate: 1500,
     animationEasingUpdate: 'quinticInOut',
     series: Object.keys(sets).filter(key => key != "undefined" && sets[key].list.length > 1).map(key => {
+        let data = sets[key].list.map(loc => {
+            return [loc.longitude, loc.latitude]
+        }).filter(d => !Number.isNaN(d[0]) || !Number.isNaN(d[0]))
+        
+        data = [
+            data.find(d => d[1] == Math.max(...data.map(d => d[1]))),
+            data.find(d => d[0] == Math.min(...data.map(d => d[0]))),
+            data.find(d => d[1] == Math.min(...data.map(d => d[1]))),
+            data.find(d => d[0] == Math.max(...data.map(d => d[0]))),
+        ];
+        console.log(data)
         return {
             name: key,
-            type: 'graph',
-            layout: 'none',
+            type: 'custom',
             coordinateSystem: 'geo',
-            symbolSize: 0 * (window.innerWidth / window.innerHeight),
-            label: {
-                show: false
-            },
-            emphasis: {
+            renderItem: function (params, api) {
+              if (params.context.rendered) {
+                return;
+              }
+              params.context.rendered = true;
+              let points = [];
+              for (let i = 0; i < data.length; i++) {
+                points.push(api.coord(data[i]));
+              }
+              let color = api.visual('color');
+              return {
+                type: 'polygon',
+                transition: ['shape'],
+                shape: {
+                  points: points,
+                  smooth: 0.5
+                },
+                style: api.style({
+                  fill: color,
+                  opacity: 0.4,
+                }),
                 focus: 'series'
+              };
             },
-            edgeSymbol: ['circle', 'none'],
-            edgeSymbolSize: [4, 10],
-            edgeLabel: {
-                fontSize: 20
-            },
-            data: sets[key].list.map(loc => {
-                return [loc.longitude, loc.latitude]
-            }),
-            links: sets[key].links,
-            lineStyle: {
-                opacity: 1,
-                width: size * (window.innerWidth / window.innerHeight),
-                curveness: 0,
-                color: 'source',
-                cap: 'round',
-                // type: 'dotted',
-            }
-        }
+            clip: true,
+            data: data
+          }
+        //     name: key,
+        //     type: 'graph',
+        //     layout: 'none',
+        //     coordinateSystem: 'geo',
+        //     symbolSize: 30 * (window.innerWidth / window.innerHeight),
+        //     label: {
+        //         show: false
+        //     },
+        //     emphasis: {
+        //         focus: 'series'
+        //     },
+        //     edgeSymbol: ['square', 'none'],
+        //     edgeSymbolSize: [4, 10],
+        //     edgeLabel: {
+        //         fontSize: 20
+        //     },
+        //     data: sets[key].list.map(loc => {
+        //         return [loc.longitude, loc.latitude]
+        //     }),
+        //     // links: sets[key].links,
+        //     lineStyle: {
+        //         opacity: 1,
+        //         width: size * (window.innerWidth / window.innerHeight),
+        //         curveness: 0,
+        //         color: 'source',
+        //         cap: 'round',
+        //         // type: 'dotted',
+        //     }
+        // }
     })
 };
 
